b1890b8c421c5be3db59bf8cb85ba752e0d152bd
Sandeep Kadoor <ksandeepa@gmail.com>
2012-02-10 16:29:28 +0530

    msg

--- app.js
+++ app.js
@@ -7,14 +7,24 @@ Ext.Loader.require('studentenrollment.view.quarterView');
 Ext.application({
 	name : 'studentenrollment',
 	//autoCreateViewport : true,
+	//renderTo: 'divTwo',
 	//height:'80',
 	//appFolder: 'app',
 	launch : function() {
-		
+		Ext.create('Ext.panel.Panel', {
+        layout: 'fit',
+        renderTo: 'divTwo', // i've added div to html
+        width: '100%',
+        height: '100%',
+        items: [
+            {
+                xtype: 'quarterView',
+                title: 'Tasks',
+                html: 'List of tasks will go here'
+            }
+        ]
+    });			
 	}
-	
 }
-
-
 );
 //Ext.Loader.setConfig({enabled:true});
